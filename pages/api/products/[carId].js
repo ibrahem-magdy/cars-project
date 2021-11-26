4bd291bf9608172aeca02ec615dbd8c056f249f9
ibrahem-magdy <hemamagdy752@gmail.com>
2021-11-26 15:52:25 +0200

    cache api

--- pages/api/products/[carId].js
+++ pages/api/products/[carId].js
@@ -2,6 +2,7 @@ import { products } from "../../../data/db";
 
 export default function handler(req, res) {
   const { carId } = req.query;
+  res.setHeader("Cache-control", "S-maxage=10,stale-while-revalidate");
   const car = products.find((car) => car.id === carId);
   res.status(200).json(car);
 }
